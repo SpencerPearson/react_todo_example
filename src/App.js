736469df35ba0b9d47f72e560ceaeb0f19c27dc9
SpencerPearson <spencer.pearson.816@outlook.com>
2023-03-04 16:58:54 -0600

    Navigation & 404 page built

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './App.css'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Navigation from './components/Navigation'
+import ToDos from './components/ToDos/ToDos'
+import Categories from './components/Auth/Categories/Categories'
+import About from './components/About/About'
+import Login from './components/Auth/Login'
+import NotFound from './components/NotFound'
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <Router>
+        <Navigation />
+        <Routes>
+          <Route path='/' element={<ToDos />} />
+          <Route path='/todos' element={<ToDos />} />
+          <Route path='/categories' element={<Categories />} />
+          <Route path='/about' element={<About />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='*' element={<NotFound />} />
+        </Routes>
+      </Router>
     </div>
-  );
+  )
 }
-
-export default App;
