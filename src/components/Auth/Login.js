5ca8aacb966af52be42e2fc2fca0557ae0a28982
SpencerPearson <spencer.pearson.816@outlook.com>
2023-03-04 18:33:23 -0600

    Brought in authentication w/ firebase

--- src/components/Auth/Login.js
+++ src/components/Auth/Login.js
@@ -1,9 +1,35 @@
 import React from 'react'
+import { useAuth } from '../../contexts/AuthContext'
+import { useNavigate } from 'react-router-dom'
+import { Container, Card } from 'react-bootstrap'
 
 export default function Login() {
+  const { login } = useAuth()
+  const navigate = useNavigate()
+
+  async function handleAuth () {
+    await login()
+
+    return navigate('/')
+  }
+
   return (
-    <h1>
-        Login
-    </h1>
+    <div className="login">
+      <article className="bg-info mb-5 p-5 text-dark">
+        <h1 className="text-center">Welcome to ReactJS ToDo!</h1>
+      </article>
+      <Container>
+        <Card className='m-2 border-dark text-center'>
+          <Card.Header className='bg-dark text-white'>
+            <h2>Login for full functionality</h2>
+          </Card.Header>
+          <Card.Body>
+            <button className='btn btn-success' onClick={() => handleAuth()}>
+              Login w/ Github
+            </button>
+          </Card.Body>
+        </Card>
+      </Container>
+    </div>
   )
 }
