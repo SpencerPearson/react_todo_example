d9ef8dd07bcdcc017ac6bc1524866de0995241cb
SpencerPearson <spencer.pearson.816@outlook.com>
2023-03-04 18:33:23 -0600

    Brought in authentication w/ firebase

--- src/App.js
+++ src/App.js
@@ -1,27 +1,33 @@
 import React from 'react'
 import './App.css'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import AuthProvider from './contexts/AuthContext'
 import Navigation from './components/Navigation'
 import ToDos from './components/ToDos/ToDos'
-import Categories from './components/Auth/Categories/Categories'
+import Categories from './components/Categories/Categories'
 import About from './components/About/About'
 import Login from './components/Auth/Login'
 import NotFound from './components/NotFound'
+import Footer from './components/Footer'
+import ProtectedRoute from './components/ProtectedRoute'
 
 export default function App() {
   return (
     <div className='App'>
-      <Router>
-        <Navigation />
-        <Routes>
-          <Route path='/' element={<ToDos />} />
-          <Route path='/todos' element={<ToDos />} />
-          <Route path='/categories' element={<Categories />} />
-          <Route path='/about' element={<About />} />
-          <Route path='/login' element={<Login />} />
-          <Route path='*' element={<NotFound />} />
-        </Routes>
-      </Router>
+      <AuthProvider>
+        <Router>
+          <Navigation />
+          <Routes>
+            <Route path='/' element={<ProtectedRoute><ToDos /></ProtectedRoute>} />
+            <Route path='/todos' element={<ProtectedRoute ><ToDos /></ProtectedRoute>} />
+            <Route path='/categories' element={<ProtectedRoute><Categories /></ProtectedRoute>} />
+            <Route path='/about' element={<About />} />
+            <Route path='/login' element={<Login />} />
+            <Route path='*' element={<NotFound />} />
+          </Routes>
+        <Footer />
+        </Router>
+      </AuthProvider>
     </div>
   )
 }
